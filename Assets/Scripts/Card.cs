85b4b813abdc6d4f5ce74f2bfcb574a852ba9f73
Nathan Shapter <shapternathan@protonmail.com>
2025-01-19 14:42:41 -0500

    deck updates
    game shows 10 of own cards, 10 of PC, and 4 cards in your hand. cards are shuffled at start. When you click on a card it satttes whether or not it is in your hand

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,6 +5,7 @@ public class Card : MonoBehaviour
     [SerializeField] int cardValue;
     [SerializeField] Sprite[] sprites; // 0 for clubs, 1 for diamond, 2 for heart, 3 for spade
     SpriteRenderer spriteRenderer;
+    public bool isInHand = false;
 
 
     private void Start()
@@ -12,6 +13,11 @@ public class Card : MonoBehaviour
      spriteRenderer = GetComponent<SpriteRenderer>();
         
         spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Card status in hand {isInHand}");
     }
 
 }
--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -1,17 +1,96 @@
+using Mono.Cecil;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
+    [SerializeField] bool playerDeck = true;
+
+
    [SerializeField] GameObject[] cardsToPickFrom;
 
-  [SerializeField]  GameObject[] deck;
+  [SerializeField]  List<GameObject> deck;
 
+    private int numberOfCardsInDeck = 26;
+
+
+    [SerializeField] Transform deckPosition;
+
+    [SerializeField] Transform[] playingCardPosition;
+
+    private int handSize = 14;
+
+    [SerializeField] Sprite backOfCardSprite;
+
+    [SerializeField] float timeInBetweenCard = 0.3f;
 
     private void Start()
     {
-        
+        for (int i = 0; i < playingCardPosition.Length; i++) // deactivates the playing cards visuals
+        {
+            playingCardPosition[i].gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < numberOfCardsInDeck; i++)  // creates the deck
+        {
+
+            GameObject cardPrefab = cardsToPickFrom[i % cardsToPickFrom.Length];
+
+            GameObject instantiatedCard = Instantiate(cardPrefab, deckPosition);
+
+            deck.Add(instantiatedCard);
+           
+            
+        }
+      
+
+        ShuffleDeck();
+
+        StartCoroutine(PlaceCardsInHand());
     }
 
+    void ShuffleDeck()
+    {
+       
+
+        for (int i = deck.Count - 1; i >= 0; i--) 
+        {
+            int randomIndex = Random.Range(0, i + 1); // gets a random number of remaining values
+
+            GameObject temp = deck[i];
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+    }
+    private IEnumerator PlaceCardsInHand()
+    {
+        int cardsToPlace = Mathf.Min(handSize, deck.Count, playingCardPosition.Length);
+        for (int i = 0; i < cardsToPlace; i++) 
+        {
+            yield return new WaitForSeconds(timeInBetweenCard);
+
+        
+
+            if(i >= 10)
+            {
+                if (!playerDeck)
+                {
+                    deck[i].gameObject.GetComponent<SpriteRenderer>().sprite = backOfCardSprite;
+                }
+                deck[i].gameObject.GetComponent<Card>().isInHand = true;
+            }
+
+
+
+            deck[i].transform.position = playingCardPosition[i].transform.position;
+        }
+       
+
+       
+       
+
+     
+    }
 }
 
