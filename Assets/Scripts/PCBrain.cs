d2ce4ea1045b21ab640503b4aff512a1c8e2d12a
Nathan Shapter <shapternathan@protonmail.com>
2025-01-19 21:38:14 -0500

    it looks for a match on every card, if there is no match, it looks for a card it can defeat (TBA)

--- Assets/Scripts/PCBrain.cs
+++ Assets/Scripts/PCBrain.cs
@@ -9,7 +9,9 @@ public class PCBrain : MonoBehaviour
 
   [SerializeField]  Card[] cardsInPCHand;
 
+    [SerializeField] GameObject[] opponentRow;
 
+    [SerializeField] Card[] cardsInOpponentFirstRow;
 
     private void Start()
     {
@@ -20,6 +22,47 @@ public class PCBrain : MonoBehaviour
         print("pc turn");
         cardsInPCHand = PCHand.GetComponentsInChildren<Card>();
 
+        cardsInOpponentFirstRow = opponentRow[0].GetComponentsInChildren<Card>();
+
+
+
+
+        FindMatches();
+
 
     }
+
+    private void FindMatches()
+    {
+        foreach (Card pcCard in cardsInPCHand)
+        {
+            bool matchFound = false;
+
+            foreach (Card playerCard in cardsInOpponentFirstRow)
+            {
+                if (pcCard.cardValue == playerCard.cardValue)
+                {
+                    print($"Match found! {pcCard.name} matches {playerCard.name}");
+                    MatchFound(playerCard, pcCard);
+                    matchFound = true;
+                }
+            }
+
+            // If no match is found for this PC card, you can look for larger cards or handle other logic
+            if (!matchFound)
+            {
+                print($"No match found for {pcCard.name}. Looking for a larger card.");
+                //LookForLargerCard(pcCard);
+            }
+        }
+    }
+
+
+    void MatchFound(Card playerCard, Card PCCard)
+    {
+        playerCard.gameObject.SetActive(false);
+        PCCard.gameObject.SetActive(false);
+      
+    }
+
 }
